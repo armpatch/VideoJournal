ef127f9b31a9652cc431d990b0f50dd1ecf99659
Aaron <ampatch@gmail.com>
2019-09-14 19:25:30 -0400

    Initial commit

---
+++ app/src/main/java/com/armpatch/android/videojournal/RecordingAdapter.java
@@ -0,0 +1,6 @@
+package com.armpatch.android.videojournal;
+
+import android.support.v7.widget.RecyclerView;
+
+public class RecordingAdapter extends RecyclerView.Adapter {
+}
---
+++ app/src/main/java/com/armpatch/android/videojournal/activities/RecordingListActivity.java
@@ -0,0 +1,13 @@
+package com.armpatch.android.videojournal.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import com.armpatch.android.videojournal.RecordingAdapter;
+
+public class RecordingListActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    private RecordingAdapter recordingAdapter;
+
+}
