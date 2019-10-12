c02cb1361f999f4de4907a462e7edaa61bf0d9c9
Aaron <ampatch@gmail.com>
2019-10-12 19:51:53 -0400

    Brought back a new RecordingEditorActivity

---
+++ app/src/main/java/com/armpatch/android/videojournal/activity/RecordingEditorActivity.java
@@ -0,0 +1,133 @@
+package com.armpatch.android.videojournal.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.armpatch.android.videojournal.R;
+import com.armpatch.android.videojournal.model.Recording;
+import com.armpatch.android.videojournal.model.RecordingFactory;
+import com.armpatch.android.videojournal.model.ThumbnailFactory;
+import com.armpatch.android.videojournal.util.PictureUtils;
+
+import java.io.File;
+import java.util.List;
+
+public class RecordingEditorActivity extends AppCompatActivity {
+
+    private static final int REQUEST_VIDEO_CAPTURE = 1;
+
+    private Recording recording;
+    private EditText recordingTitleText, notesText;
+    Button createButton;
+    ImageView thumbnailView;
+
+    public static Intent getIntent(Context activityContext) {
+        return new Intent(activityContext, RecordingEditorActivity.class);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.recording_editor_activity);
+
+        thumbnailView = findViewById(R.id.thumbnail);
+        recordingTitleText = findViewById(R.id.recording_title);
+        notesText = findViewById(R.id.notes);
+        createButton = findViewById(R.id.create_button);
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveRecording();
+                finish();
+            }
+        });
+
+        takeVideo();
+    }
+
+    private void setThumbnail() {
+        Bitmap thumbnail = PictureUtils.getScaledBitmap(
+                recording.getThumbnailPath(),
+                400,
+                400); // TODO: dimensions probably shouldn't be hard coded here
+
+        RoundedBitmapDrawable dr = RoundedBitmapDrawableFactory.create(thumbnailView.getResources(), thumbnail);
+
+        dr.setCornerRadius(4);
+        thumbnailView.setImageDrawable(dr);
+    }
+
+    private void takeVideo() {
+        recording = new Recording();
+
+        File outputFile = new File(getFilesDir(), recording.getVideoFilename());
+        recording.setVideoPath(outputFile.getAbsolutePath());
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        Uri outputUri = FileProvider.getUriForFile(this, "com.armpatch.android.videojournal.fileprovider", outputFile);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
+
+        // grant uri permissions
+        List<ResolveInfo> cameraActivities = getPackageManager().
+                queryIntentActivities(cameraIntent, PackageManager.MATCH_DEFAULT_ONLY);
+
+        for (ResolveInfo activity : cameraActivities) {
+            grantUriPermission(activity.activityInfo.packageName, outputUri,
+                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        }
+
+        // if a camera is available, start it
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(cameraIntent, REQUEST_VIDEO_CAPTURE);
+        }
+    }
+
+
+    private void saveRecording() {
+        String titleUserEntered = recordingTitleText.getText().toString();
+
+        if (titleUserEntered.isEmpty()) {
+            recording.title = "New Recording";
+        } else {
+            recording.title = titleUserEntered;
+        }
+
+        recording.notes = notesText.getText().toString();
+
+        RecordingFactory.get(this).addRecording(recording);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            recording.setThumbnailPath(ThumbnailFactory.createThumbnail(this, recording));
+
+            setThumbnail();
+
+
+        }
+
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode != RESULT_OK) {
+            finish();
+        }
+    }
+}
--- app/src/main/java/com/armpatch/android/videojournal/activity/RecordingListActivity.java
+++ app/src/main/java/com/armpatch/android/videojournal/activity/RecordingListActivity.java
@@ -1,53 +1,38 @@
 package com.armpatch.android.videojournal.activity;
 
 import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.armpatch.android.videojournal.R;
-import com.armpatch.android.videojournal.dialog.RecordingEditorDialog;
 import com.armpatch.android.videojournal.dialog.RecordingViewerDialog;
 import com.armpatch.android.videojournal.model.Recording;
 import com.armpatch.android.videojournal.model.RecordingFactory;
 import com.armpatch.android.videojournal.recyclerview.RecordingAdapter;
 import com.armpatch.android.videojournal.recyclerview.RecordingHolder;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RecordingListActivity extends AppCompatActivity implements RecordingHolder.Callbacks {
-
-    private static final int REQUEST_VIDEO_CAPTURE = 1;
 
     RecyclerView recyclerView;
     FloatingActionButton fab;
 
     private RecordingAdapter recordingAdapter;
 
-    Recording recordingSlate;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recording_list_activity);
-
-        checkPermissions();
 
         recyclerView = findViewById(R.id.recycler_view);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
@@ -57,9 +42,39 @@ public class RecordingListActivity extends AppCompatActivity implements Recordin
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                takeVideo();
+                Intent intent = RecordingEditorActivity.getIntent(RecordingListActivity.this);
+                startActivity(intent);
             }
         });
+
+        askForPermissions();
+    }
+
+    private void askForPermissions() {
+        String[] permissions = new String[]{
+                Manifest.permission.INTERNET,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.VIBRATE,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.CAPTURE_VIDEO_OUTPUT,
+                Manifest.permission.CAMERA
+
+        };
+
+        int result;
+        List<String> permissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = checkSelfPermission(p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                permissionsNeeded.add(p);
+            }
+        }
+
+        if (!permissionsNeeded.isEmpty()) {
+            requestPermissions(permissionsNeeded.toArray(new String[permissionsNeeded.size()]), 100);
+        }
     }
 
     @Override
@@ -87,73 +102,4 @@ public class RecordingListActivity extends AppCompatActivity implements Recordin
         dialog.show();
     }
 
-    private void checkPermissions() {
-        String[] permissions = new String[]{
-                Manifest.permission.INTERNET,
-                Manifest.permission.READ_PHONE_STATE,
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.VIBRATE,
-                Manifest.permission.RECORD_AUDIO,
-                Manifest.permission.CAPTURE_VIDEO_OUTPUT,
-                Manifest.permission.CAMERA
-
-        };
-
-        int result;
-        List<String> permissionsNeeded = new ArrayList<>();
-        for (String p : permissions) {
-            result = checkSelfPermission(p);
-            if (result != PackageManager.PERMISSION_GRANTED) {
-                permissionsNeeded.add(p);
-            }
-        }
-
-        if (!permissionsNeeded.isEmpty()) {
-            requestPermissions(permissionsNeeded.toArray(new String[permissionsNeeded.size()]), 100);
-        }
-    }
-
-    private void takeVideo() {
-        Intent cameraIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-
-        recordingSlate = new Recording();
-
-        File outputFile = new File(getFilesDir(), recordingSlate.getVideoFilename());
-        recordingSlate.setVideoPath(outputFile.getAbsolutePath());
-
-        Uri outputUri = FileProvider.getUriForFile(this, "com.armpatch.android.videojournal.fileprovider", outputFile);
-
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
-
-        // grant uri permissions
-        List<ResolveInfo> cameraActivities = getPackageManager().
-                queryIntentActivities(cameraIntent, PackageManager.MATCH_DEFAULT_ONLY);
-
-        for (ResolveInfo activity : cameraActivities) {
-            grantUriPermission(activity.activityInfo.packageName, outputUri,
-                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-        }
-
-        // if a camera is available, start it
-        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(cameraIntent, REQUEST_VIDEO_CAPTURE);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
-
-            RecordingEditorDialog dialog = new RecordingEditorDialog(this, recordingSlate);
-            dialog.show();
-            dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                @Override
-                public void onDismiss(DialogInterface dialog) {
-                    updateRecyclerView();
-                }
-            });
-        }
-    }
 }
